cc9364eba7817cfd181f247973ef7026c98457e5
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-16 03:51:52 +0900

    Update __init__.py

--- azely/__init__.py
+++ azely/__init__.py
@@ -1,1 +1,17 @@
 # coding: utf-8
+
+# standard library
+import os
+
+# dependent packages
+import yaml
+
+# submodules
+from .utils import *
+from .locations import *
+
+# information
+__version__ = '0.1'
+__author__  = 'Akio Taniguchi'
+__license__ = 'MIT'
+
