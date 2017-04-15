b8896bf7f0fad14f371d32f63a18d15327b0ebc6
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-16 03:52:33 +0900

    Add class 'AzelyError'

--- azely/utils.py
+++ azely/utils.py
@@ -1,1 +1,14 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['AzelyError']
+
+
+# classes
+class AzelyError(Exception):
+    """Error class of Azely."""
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
