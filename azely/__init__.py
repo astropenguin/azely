810e60386d3f6f36ca76e7c75dfa9e88472dc2b7
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-16 03:52:01 +0900

    Update __init__.py

--- azely/__init__.py
+++ azely/__init__.py
@@ -15,3 +15,15 @@ __version__ = '0.1'
 __author__  = 'Akio Taniguchi'
 __license__ = 'MIT'
 
+# constants
+HOME_DIR = os.environ['HOME']
+AZELY_DIR = os.path.join(HOME_DIR, '.azely')
+KNOWN_LOCS = os.path.join(AZELY_DIR, 'known_locations.yaml')
+
+# create directory and files (if not exist)
+if not os.path.exists(AZELY_DIR):
+    os.makedirs(AZELY_DIR)
+
+if not os.path.exists(KNOWN_LOCS):
+    with open(KNOWN_LOCS, 'w') as f:
+        f.write(yaml.dump({}, default_flow_style=False))
