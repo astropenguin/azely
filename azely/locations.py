5e51d4ee010585259b10e1c0194463e1ddd95614
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-16 22:13:48 +0900

    Update function 'get_location', 'request_location', 'update_known_locations'

--- azely/locations.py
+++ azely/locations.py
@@ -2,8 +2,8 @@
 
 # imported items
 __all__ = [
-    'request_location',
     'get_location',
+    'request_location',
 ]
 
 # standard library
@@ -22,67 +22,67 @@ URL_TIMEZONE = URL_API + '/timezone/json?location={0},{1}&timestamp={2}'
 
 
 # functions
-def request_location(location, date, encoding='utf-8', timeout=5):
-    # get geocode info from google maps
-    url = URL_GEOCODE.format(location)
-    with urlopen(url, timeout=timeout) as f:
-        geocode = yaml.load(f.read().decode(encoding))['results'][0]
-
-    info = {}
-    info['name']      = geocode['address_components'][0]['long_name']
-    info['address']   = geocode['formatted_address']
-    info['latitude']  = geocode['geometry']['location']['lat']
-    info['longitude'] = geocode['geometry']['location']['lng']
-    info['query']     = location
-
-    # get timezone info from google maps
-    t = (date-datetime(1970, 1, 1)).total_seconds() # UNIX time
-    url = URL_TIMEZONE.format(info['latitude'], info['longitude'], t)
-    with urlopen(url, timeout=timeout) as f:
-        timezone = yaml.load(f.read().decode(encoding))
-
-    info['timezone_name'] = timezone['timeZoneName']
-    info['timezone_hour'] = (timezone['rawOffset']+timezone['dstOffset']) / 3600
-    info['timezone_date'] = date.strftime('%Y-%m-%d')
-
-    return info
-
-
-def update_known_locations(location, info):
+def get_location(name, date):
     with open(azely.KNOWN_LOCS, 'r') as f:
-        dinfo = yaml.load(f)
+        locations = yaml.load(f)
 
-    if location in dinfo:
-        dinfo[location]['timezone_name'] = info['timezone_name']
-        dinfo[location]['timezone_hour'] = info['timezone_hour']
-        dinfo[location]['timezone_date'] = info['timezone_date']
-    else:
-        dinfo[location] = info
-
-    with open(azely.KNOWN_LOCS, 'w') as f:
-        f.write(yaml.dump(dinfo, default_flow_style=False))
-
-
-def get_location(location, date):
-    with open(azely.KNOWN_LOCS, 'r') as f:
-        dinfo = yaml.load(f)
-
-    if location in dinfo:
-        info = dinfo[location]
-        if 'query' in info:
+    if name in locations:
+        loc = locations[name]
+        if 'query' in loc:
             try:
-                info = request_location(info['query'], date)
-                update_known_locations(location, info)
+                loc = request_location(loc['query'], date)
+                update_known_locations(name, loc)
             except URLError:
-                if not info['timezone_day'] == date.strftime('%Y-%m-%d'):
+                if not loc['timezone_day'] == date.strftime('%Y-%m-%d'):
                     print('AzelyWarning: timezone hour might be different')
         else:
             print('AzelyWarning: location is not updated (no place_id)')
     else:
         try:
-            info = request_location(location, date)
-            update_known_locations(location, info)
+            loc = request_location(name, date)
+            update_known_locations(name, loc)
         except URLError:
             raise azely.AzelyError('error!')
 
-    return info
+    return loc
+
+
+def request_location(name, date, encoding='utf-8', timeout=5):
+    # get geocode from google maps api
+    url = URL_GEOCODE.format(name)
+    with urlopen(url, timeout=timeout) as f:
+        geocode = yaml.load(f.read().decode(encoding))['results'][0]
+
+    loc = {}
+    loc['name']      = geocode['address_components'][0]['long_name']
+    loc['address']   = geocode['formatted_address']
+    loc['latitude']  = geocode['geometry']['location']['lat']
+    loc['longitude'] = geocode['geometry']['location']['lng']
+    loc['query']     = name
+
+    # get timezone from google maps api
+    t = (date-datetime(1970, 1, 1)).total_seconds() # UNIX time
+    url = URL_TIMEZONE.format(loc['latitude'], loc['longitude'], t)
+    with urlopen(url, timeout=timeout) as f:
+        timezone = yaml.load(f.read().decode(encoding))
+
+    loc['timezone_name'] = timezone['timeZoneName']
+    loc['timezone_hour'] = (timezone['rawOffset']+timezone['dstOffset']) / 3600
+    loc['timezone_date'] = date.strftime('%Y-%m-%d')
+
+    return loc
+
+
+def update_known_locations(name, loc):
+    with open(azely.KNOWN_LOCS, 'r') as f:
+        locations = yaml.load(f)
+
+    if name in locations:
+        locations[name]['timezone_name'] = loc['timezone_name']
+        locations[name]['timezone_hour'] = loc['timezone_hour']
+        locations[name]['timezone_date'] = loc['timezone_date']
+    else:
+        locations[name] = loc
+
+    with open(azely.KNOWN_LOCS, 'w') as f:
+        f.write(yaml.dump(locations, default_flow_style=False))
