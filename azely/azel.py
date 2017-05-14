51b065fe598cf7a327311bf87837ba7317e120a3
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-05-14 16:45:06 +0900

    Add class 'AzEl'

--- azely/azel.py
+++ azely/azel.py
@@ -1,1 +1,73 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['AzEl']
+
+# standard library
+
+# dependent packages
+import azely
+import ephem
+import numpy as np
+from numpy import rec
+
+# constants
+UT_TO_LST = 1.0027379
+
+
+# classes
+class AzEl(object):
+    def __init__(self, observer, timezone=None, date=None):
+        self.info = {
+            'observer': observer,
+            'timezone': timezone or observer,
+            'date': azely.parse_date(date),
+        }
+
+    def __call__(self, obj, hr=None):
+        # body
+        body = azely.parse_object(obj)
+
+        # observer
+        observer = ephem.Observer()
+        observer.lat = str(self.info['observer']['latitude'])
+        observer.lon = str(self.info['observer']['longitude'])
+
+        if hr is None:
+            observer.date = ephem.now()
+        else:
+            observer.date = ephem.Date(self.info['date'])
+
+            if self.info['timezone'] == 'LST':
+                st = observer.sidereal_time()
+                offset_hr = st / (2*np.pi) * 24 / UT_TO_LST
+            elif type(self.info['timezone']) in (int, float):
+                offset_hr = self.info['timezone']
+            else:
+                offset_hr = self.info['timezone']['timezone_hour']
+
+            observer.date -= offset_hr * ephem.hour
+
+        # compute
+        coords = []
+        names = ['Az', 'El', 'RA', 'Dec', 'LST']
+        hrs = [hr] if not hasattr(hr, '__iter__') else hr
+        for hr in hrs:
+            coords.append(self._compute(body, observer, hr))
+
+        return rec.fromarrays(np.array(coords).T, names=names)
+
+    def _compute(self, body, observer, hr=None):
+        observer = observer.copy()
+
+        if hr is not None:
+            if self.info['timezone'] == 'LST':
+                observer.date += hr * ephem.hour / UT_TO_LST
+            else:
+                observer.date += hr * ephem.hour
+
+        body.compute(observer)
+        az, el  = np.rad2deg([body.az, body.alt])
+        ra, dec = np.rad2deg([body.ra, body.dec])
+        lst = observer.sidereal_time() / (2*np.pi) * 24
+        return az, el, ra, dec, lst
