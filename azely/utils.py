a73ac832ca68ff086904929737a2281c62ec13c8
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-05-14 16:47:42 +0900

    Add class 'AzelyWarning'

--- azely/utils.py
+++ azely/utils.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 # imported items
-__all__ = ['AzelyError']
+__all__ = [
+    'AzelyError',
+    'AzelyWarning',
 
 
 # classes
@@ -12,3 +14,14 @@ class AzelyError(Exception):
 
     def __str__(self):
         return repr(self.message)
+
+
+class AzelyWarning(Warning):
+    """Warning class of Azely."""
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
