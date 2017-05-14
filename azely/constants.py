f8414a49558a8c5ca0a7f0fcf13a043dc344ccbe
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-05-14 16:44:43 +0900

    Add constant 'HOME_DIR', 'AZELY_DIR', 'DATA_DIR', 'KNOWN_LOCS'

--- azely/constants.py
+++ azely/constants.py
@@ -1,1 +1,20 @@
 # coding: utf-8
+
+# imported items
+__all__ = [
+    'AZELY_DIR',
+    'DATA_DIR',
+    'KNOWN_LOCS',
+]
+
+# standard library
+import os
+
+# dependent packages
+import azely
+
+# constants
+HOME_DIR = os.environ['HOME']
+AZELY_DIR = os.path.join(HOME_DIR, '.azely')
+DATA_DIR = os.path.join(azely.__path__[0], 'data')
+KNOWN_LOCS = os.path.join(AZELY_DIR, 'known_locations.yaml')
