661395d7656ef2e488e4a4aeea241699effd24f6
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-05-14 16:46:45 +0900

    Add class 'ObjectLists'

--- azely/objects.py
+++ azely/objects.py
@@ -1,1 +1,49 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['ObjectLists']
+
+# standard library
+import os
+from collections import OrderedDict
+from copy import deepcopy
+from glob import glob
+from pprint import pformat
+
+# dependent packages
+import azely
+import ephem
+import yaml
+import numpy as np
+
+yaml.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+    lambda loader, node: OrderedDict(loader.construct_pairs(node))
+)
+
+
+# classes
+class ObjectLists(dict):
+    def __init__(self):
+        super().__init__()
+
+        # azely data directorVy
+        for fname in glob(os.path.join(azely.DATA_DIR, '*.yaml')):
+            with open(fname, 'r') as f:
+                self.update(yaml.load(f))
+
+        # ~/.azely directory
+        for fname in glob(os.path.join(azely.AZELY_DIR, '*.yaml')):
+            if fname == azely.KNOWN_LOCS:
+                continue
+
+            with open(fname, 'r') as f:
+                self.update(yaml.load(f))
+
+        # current directory
+        for fname in glob('*.yaml'):
+            with open(fname, 'r') as f:
+                self.update(yaml.load(f))
+
+    def __repr__(self):
+        return pformat(dict(self))
