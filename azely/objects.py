f8b7cfe5c421be6486de1c0ae6b995eb460d4e9e
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-06-18 22:52:21 +0900

    Update class 'Objects'

--- azely/objects.py
+++ azely/objects.py
@@ -42,5 +42,24 @@ class Objects(dict):
             with open(fname, 'r') as f:
                 self.update(yaml.load(f))
 
+    def __getitem__(self, name):
+        item = OrderedDict()
+        for obj in azely.parse_objects(name):
+            if obj in self:
+                value = super().__getitem__(obj)
+                if issubclass(type(value), dict):
+                    if not any(['ra' in value, 'dec' in value]):
+                        item.update(value)
+                        continue
+
+                item.update({obj: value})
+            else:
+                item.update({obj: obj})
+                for value in self.values():
+                    if issubclass(type(value), dict):
+                        item.update({obj: value.get(obj, obj)})
+
+        return item
+
     def __repr__(self):
         return pformat(dict(self))
