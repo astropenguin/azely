26cc4ec3c27809fc89089270417adb070574a393
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-22 21:46:42 +0900

    Update dayplot_simple.py

--- examples/dayplot_simple.py
+++ examples/dayplot_simple.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-'''Day plot of the Sun's elevation today.'''
+"""Day plot of the Sun's elevation today."""
 
 import azely
 import numpy as np
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 
 # calculating
 azel = azely.AzEl('Mitaka')
-t = np.arange(0, 24, 0.01)
+t = np.arange(0, 24.01, 0.01)
 el = azel('Sun', t).el
 
 # plotting
 plt.plot(t, el)
 plt.xlim([0, 24])
 plt.ylim([0, 90])
-plt.title(azel['location']['name'])
-plt.xlabel(azel['timezone']['timezone_name'])
-plt.ylabel('Elevation')
+plt.title('{0} / {1}'.format(azel.location['name'], azel.date))
+plt.xlabel('{0} (hr)'.format(azel.timezone['timezone_name']))
+plt.ylabel('Elevation (deg)')
 plt.show()
