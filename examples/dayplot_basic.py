1205ed5b5db55ecff5ca82b19cc80ae76e3b52b8
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-22 21:46:15 +0900

    Update dayplot_basic.py

--- examples/dayplot_basic.py
+++ examples/dayplot_basic.py
@@ -1,32 +1,31 @@
 # coding: utf-8
 
-'''Day plot of the solar objects' elevations with given date, location, and timezone.'''
+"""Day plot of the solar objects' elevations with given date, location, and timezone."""
 
 import azely
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # date and locations
-date = '2017.05.15' # optional: None
-location = 'alma observatory'
-timezone = 'japan' # optional: None, 9.0, 'LST'
+date = '2017-01-01' # optional: None
+location = 'ALMA Observatory'
+timezone = 'Mitaka' # optional: None, 9.0, 'LST'
 
 # objects
-objs = azely.Objects()['solar']
+objs = azely.Objects()['Solar']
 
 # calculating and plotting
 azel = azely.AzEl(location, timezone, date)
-t = np.arange(0, 24, 0.01)
+t = np.arange(0, 24.01, 0.01)
 
-for name, obj in objs.items():
+for label, obj in objs.items():
     el = azel(obj, t).el
-    plt.plot(t, el, label=name)
+    plt.plot(t, el, label=label)
 
 plt.xlim([0, 24])
 plt.ylim([0, 90])
-plt.title(azel['location']['name'])
-plt.xlabel(azel['timezone']['timezone_name'])
-plt.ylabel('Elevation')
+plt.title('{0} / {1}'.format(azel.location['name'], azel.date))
+plt.xlabel('{0} (hr)'.format(azel.timezone['timezone_name']))
+plt.ylabel('Elevation (deg)')
 plt.legend()
 plt.show()
