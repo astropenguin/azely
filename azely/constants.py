e05707ff816a980ae6a6c84a6db558e9e370b8fa
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-22 18:10:12 +0900

    Add constant 'SAMPLE_OBJS'

--- azely/constants.py
+++ azely/constants.py
@@ -5,6 +5,7 @@ __all__ = [
     'AZELY_DIR',
     'DATA_DIR',
     'KNOWN_LOCS',
+    'SAMPLE_OBJS',
 ]
 
 # standard library
@@ -14,6 +15,7 @@ import os
 import azely
 
 # local constants
-AZELY_DIR = os.path.join(os.environ['HOME'], '.azely')
-DATA_DIR = os.path.join(azely.__path__[0], 'data')
-KNOWN_LOCS = os.path.join(AZELY_DIR, 'known_locations.yaml')
+AZELY_DIR   = os.path.join(os.environ['HOME'], '.azely')
+DATA_DIR    = os.path.join(azely.__path__[0], 'data')
+KNOWN_LOCS  = os.path.join(AZELY_DIR, 'known_locations.yaml')
+SAMPLE_OBJS = os.path.join(DATA_DIR, 'sample_objects.yaml')
