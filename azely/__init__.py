3ec29d98f2d164e015825ad24c413858aba5c316
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-01-12 18:21:38 +0900

    Update __init__.py

--- azely/__init__.py
+++ azely/__init__.py
@@ -1,15 +1,8 @@
 # coding: utf-8
 
 # information
-__version__ = '0.1'
+__version__ = '0.2'
 __author__  = 'snoopython'
-
-# standard library
-import os
-import shutil
-
-# dependent packages
-import yaml
 
 # submodules
 from .azel import *
@@ -19,19 +12,7 @@ from .objects import *
 from .plotting import *
 from .utils import *
 
-# create directory and file (if not existing)
-if not os.path.exists(AZELY_DIR):
-    os.makedirs(AZELY_DIR)
-    shutil.copy(SAMPLE_OBJS, AZELY_DIR)
-
-if not os.path.exists(KNOWN_LOCS):
-    with open(KNOWN_LOCS, 'w') as f:
-        f.write(yaml.dump({}, default_flow_style=False))
-
 # delete items
-del os
-del shutil
-del yaml
 del azel
 del constants
 del locations
