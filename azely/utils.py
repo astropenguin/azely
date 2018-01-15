58c1182b19b8c40b873cee7b4ed48a718f52fc7b
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-01-15 17:01:00 +0900

    Update utils.py

--- azely/utils.py
+++ azely/utils.py
@@ -4,23 +4,32 @@
 __all__ = ['get_body',
            'islst',
            'isnumber',
+           'read_yaml',
+           'write_yaml',
            'open_googlemaps',
            'parse_date']
 
 # standard library
 import re
 import webbrowser
+from collections import OrderedDict
 from datetime import datetime
 from urllib.parse import urlencode
 
 # dependent packages
 import azely
 import ephem
+import yaml
 from astropy.coordinates import SkyCoord
 
 # module constants
 URL_GOOGLEMAPS = 'https://www.google.com/maps'
 
+# use OrderedDict in PyYAML by default
+yaml.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+    lambda loader, node: OrderedDict(loader.construct_pairs(node))
+)
 
 # functions
 def get_body(object_like):
@@ -60,6 +69,39 @@ def isnumber(obj):
         return True
     except ValueError:
         return False
+
+
+def read_yaml(filepath, keep_order=False):
+    with filepath.open('r') as f:
+        try:
+            if keep_order:
+                result = yaml.load(f)
+            else:
+                Loader = yaml.loader.SafeLoader
+                result = yaml.load(f, Loader)
+        except:
+            # fail to load yaml
+            print('logging later!')
+            return {}
+
+    if not result:
+        # empty file
+        return {}
+    else:
+        # valid yaml
+        return result
+
+
+def write_yaml(filepath, data, flow_style=False):
+    with filepath.open('w') as f:
+        try:
+            if flow_style:
+                f.write(yaml.dump(data))
+            else:
+                f.write(yaml.dump(data, default_flow_style=False))
+        except:
+            # fail to write yaml
+            print('logging later!')
 
 
 def open_googlemaps(location):
