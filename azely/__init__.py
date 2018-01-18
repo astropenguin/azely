50d9fc84344d764395fdaa005e67ce9e073d2550
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-01-18 21:23:47 +0900

    Fix __init__.py

--- azely/__init__.py
+++ azely/__init__.py
@@ -5,20 +5,20 @@ __version__ = '0.2'
 __author__  = 'snoopython'
 
 # submodules
+from .utils import *
 from .azel import *
 from .constants import *
 from .locations import *
 from .objects import *
 from .plotting import *
-from .utils import *
 
 # delete items
+del utils
 del azel
 del constants
 del locations
 del objects
 del plotting
-del utils
 
 # for convenience
 objects = Objects()
