4d191f7d13a67dd1b24b07c37f8926da6bad19e7
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-02-02 14:02:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
-"""Python 3 tool to plot azimuth and elevation of astronomical objects"""
-
-# standard library
 from setuptools import setup
 
-
-# functions
-setup(
-    name = 'azely',
-    description = __doc__,
-    version = '0.2',
-    author = 'snoopython',
-    author_email = 'taniguchi@ioa.s.u-tokyo.ac.jp',
-    url = 'https://github.com/snoopython/azely',
-    keywords = 'astronomy visualization python',
-    packages = ['azely'],
-    package_data = {'azely': ['data/*.yaml']},
-    entry_points = {'console_scripts': ['azely=azely.cli:main']},
-    install_requires = ['docopt', 'pyephem', 'pyyaml'],
-)
+setup(name = 'azely',
+      description = 'Calculate azimuth/elevation of astronomical objects',
+      version = '0.2',
+      author = 'snoopython',
+      author_email = 'taniguchi@ioa.s.u-tokyo.ac.jp',
+      url = 'https://github.com/snoopython/azely',
+      keywords = 'astronomy visualization python azimuth elevation',
+      packages = ['azely'],
+      package_data = {'azely': ['data/*.yaml']},
+      entry_points = {'console_scripts': ['azely=azely.cli:main']},
+      install_requires = ['astropy', 'numpy', 'pyyaml'])
