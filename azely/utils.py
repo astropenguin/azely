9c47bd7580ec6a50a97f4d056540343f08c4c39c
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-02-28 23:39:35 +0900

    Fix: missing right parenthesis

--- azely/utils.py
+++ azely/utils.py
@@ -218,7 +218,7 @@ def flatten(sequence, depth=None, *, exclude_classes=(str, dict)):
 
     if (depth+1
         and hasattr(sequence, '__iter__')
-        and not isinstance(sequence, exclude_classes):
+        and not isinstance(sequence, exclude_classes)):
         # recursively flatten each element
         for element in sequence:
             yield from flatten(element, depth-1,
