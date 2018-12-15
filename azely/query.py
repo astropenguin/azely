9079daf3483a206b1fa31939bd44837b58ec248d
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-12-16 00:41:28 +0900

    :sparkles: Add object query functions

--- azely/query.py
+++ azely/query.py
@@ -1,9 +1,9 @@
 __all__ = ['get_location',
-           'get_geometry',
-           'get_timezone']
+           'get_object']
 
 
 # standard library
+from copy import deepcopy
 from time import mktime
 from pathlib import Path
 from logging import getLogger
@@ -15,6 +15,7 @@ import azely
 import googlemaps
 
 
+# main query functions
 def get_location(query=None, date=None, **kwargs):
     geo = get_geometry(query, **kwargs)
 
@@ -25,8 +26,18 @@ def get_location(query=None, date=None, **kwargs):
         return geo
 
 
-@azely.cache_to(azely.config['caches']['locations'])
-@azely.default_kwargs(azely.config['locations'])
+@azely.default_kwargs(azely.config['object'])
+def get_object(query, **kwargs):
+    if azely.is_solar(query):
+        return {'name': query, 'solar': True}
+
+    try:
+        return get_local(query, **kwargs)
+    except ValueError:
+        return get_remote(query, **kwargs)
+
+
+# sub query functions
 def get_geometry(query=None, **kwargs):
     client = googlemaps.Client(**kwargs)
 
@@ -77,3 +88,44 @@ def get_timezone(query=None, date=None, **kwargs):
     tz_hour = (result['rawOffset']+result['dstOffset']) / 3600
 
     return {'timezone_name': tz_name, 'timezone_hour': tz_hour}
+
+
+@azely.cache_to(azely.config['cache']['object'],
+                azely.config['cache']['enable'])
+def get_remote(query, frame='icrs', timeout=5, **kwargs):
+    from astropy.utils.data import Conf
+    from astropy.coordinates import SkyCoord
+    from astropy.coordinates.name_resolve import NameResolveError
+
+    try:
+        with Conf.remote_timeout.set_temp(timeout):
+            coord = SkyCoord.from_name(query, frame)
+            ra, dec = coord.to_string('hmsdms').split()
+    except NameResolveError:
+        raise ValueError(query)
+
+    return {'name': query, 'solar': False,
+            'ra': ra, 'dec': dec, 'frame': frame}
+
+
+def get_local(query, pattern='*.toml', searchdirs=['.'], **kwargs):
+    from astropy.coordinates import SkyCoord
+
+    for searchdir in (Path(d).expanduser() for d in searchdirs):
+        for path in searchdir.glob(pattern):
+            data = azely.read_toml(path)
+
+            if query not in data:
+                continue
+
+            try:
+                obj = deepcopy(data[query])
+                obj.pop('name', None)
+                obj.pop('solar', None)
+                SkyCoord(**obj)
+            except:
+                continue
+
+            return data[query]
+
+    raise ValueError(query)
