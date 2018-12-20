219bd11650717e8bef6634f2aadae67b614e9ced
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-12-21 02:02:48 +0900

    :sparkles: Add AzEl class

--- azely/azel.py
+++ azely/azel.py
@@ -1,1 +1,24 @@
-__all__ = []
+__all__ = ['Observer']
+
+
+# standard library
+from logging import getLogger
+logger = getLogger(__name__)
+
+# dependent packages
+import azely
+
+
+class Observer:
+    def __init__(self, query=None, date=None, **kwargs):
+        self.date = azely.parse_date(date)
+        self.location = azely.get_location(query, date, **kwargs)
+
+    def observe(self, query, **kwargs):
+        obj = azely.get_object(query, **kwargs)
+        return azely.AzEl.from_observer(obj, self.location, self.date)
+
+    def __repr__(self):
+        name = self.location['name']
+        date = self.date.strftime('%Y-%m-%d')
+        return f'Observer({name} / {date})'
