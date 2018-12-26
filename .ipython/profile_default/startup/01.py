f719c848d295e0b1b34b15222033a7592e67e0d0
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-12-26 14:53:30 +0900

    :+1: Update IPython config script

--- .ipython/profile_default/startup/01.py
+++ .ipython/profile_default/startup/01.py
@@ -2,9 +2,18 @@
 import re
 import os
 import sys
+import time
+import json
+import toml
 from pathlib import Path
+from datetime import datetime
 
 # dependent packages
 import azely
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import AltAz, EarthLocation
