3626bcc1b7982f70c9bbf36521f20000705ef451
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-12-26 14:13:56 +0900

    :+1: Update query functions

--- azely/query.py
+++ azely/query.py
@@ -3,7 +3,6 @@ __all__ = ['get_location',
 
 
 # standard library
-from time import mktime
 from pathlib import Path
 from logging import getLogger
 logger = getLogger(__name__)
@@ -14,19 +13,16 @@ import azely
 import googlemaps
 from astropy.utils.data import Conf
 from astropy.coordinates import SkyCoord
-from astropy.coordinates.name_resolve import NameResolveError
+from astropy.coordinates import name_resolve
 
 
 # main query functions
 @azely.default_kwargs(azely.config['location'])
-def get_location(query=None, date=None, **kwargs):
-    geo = get_geometry(query, **kwargs)
-
-    try:
-        tz = get_timezone(query, date, **kwargs)
-        return {**geo, **tz}
-    except:
-        return geo
+def get_location(query=None, **kwargs):
+    if query is None:
+        return geolocate(**kwargs)
+    else:
+        return find_place(query, **kwargs)
 
 
 @azely.default_kwargs(azely.config['object'])
@@ -35,79 +31,72 @@ def get_object(query, **kwargs):
         return {'name': query}
 
     try:
-        return get_local(query, **kwargs)
+        return from_local(query, **kwargs)
     except ValueError:
-        return get_remote(query, **kwargs)
+        return from_remote(query, **kwargs)
 
 
 # sub query functions
-@azely.cache_to(azely.config['cache']['location'],
-                azely.config['cache']['enable'])
-def get_geometry(query=None, **kwargs):
+def geolocate(**kwargs):
     client = googlemaps.Client(**kwargs)
 
     # coordinates
-    if query is None:
-        result = client.geolocate()
-        name = 'Current Location'
-        addr = ''
-        pid  = ''
-        lat  = result['location']['lat']
-        lng  = result['location']['lng']
-    else:
-        result = client.places(query)['results'][0]
-        name = result['name']
-        addr = result['formatted_address']
-        pid  = result['place_id']
-        lat  = result['geometry']['location']['lat']
-        lng  = result['geometry']['location']['lng']
+    result = client.geolocate()
+    name = 'Current Location'
+    addr = ''
+    lat = result['location']['lat']
+    lng = result['location']['lng']
 
     # altitude
     result = client.elevation((lat, lng))[0]
     alt = result['elevation']
 
-    # timezone (DST is not considered)
-    t_unix = mktime(azely.parse_date().timetuple())
-    result = client.timezone((lat, lng), t_unix)
-    tz_name = result['timeZoneId']
-    tz_hour = result['rawOffset'] / 3600
+    # timezone
+    result = client.timezone((lat, lng))
+    tz = result['timeZoneId']
 
-    return {'name': name, 'address': addr, 'place_id': pid,
-            'latitude': lat, 'longitude': lng, 'altitude': alt,
-            'timezone_name': tz_name, 'timezone_hour': tz_hour}
+    return {'name': name, 'address': addr, 'timezone': tz,
+            'latitude': lat, 'longitude': lng, 'altitude': alt}
 
 
-def get_timezone(query=None, date=None, **kwargs):
+@azely.cache_to(azely.config['cache']['location'],
+                azely.config['cache']['enable'])
+def find_place(query, **kwargs):
     client = googlemaps.Client(**kwargs)
 
-    geo = get_geometry(query, **kwargs)
-    lat, lng = geo['latitude'], geo['longitude']
+    # coordinates
+    result = client.places(query)['results'][0]
+    name = result['name']
+    addr = result['formatted_address']
+    lat = result['geometry']['location']['lat']
+    lng = result['geometry']['location']['lng']
 
-    tz_date = azely.parse_date(date)
-    t_unix = mktime(tz_date.timetuple())
-    result = client.timezone((lat, lng), t_unix)
-    tz_name = result['timeZoneName']
-    tz_hour = (result['rawOffset']+result['dstOffset']) / 3600
+    # altitude
+    result = client.elevation((lat, lng))[0]
+    alt = result['elevation']
 
-    return {'timezone_name': tz_name,
-            'timezone_hour': tz_hour,
-            'timezone_date': tz_date}
+    # timezon
+    result = client.timezone((lat, lng))
+    tz = result['timeZoneId']
+
+    return {'name': name, 'address': addr, 'timezone': tz,
+            'latitude': lat, 'longitude': lng, 'altitude': alt}
 
 
 @azely.cache_to(azely.config['cache']['object'],
                 azely.config['cache']['enable'])
-def get_remote(query, frame='icrs', timeout=5, **kwargs):
+def from_remote(query, frame='icrs', timeout=5, **kwargs):
     try:
         with Conf.remote_timeout.set_temp(timeout):
             coord = SkyCoord.from_name(query, frame)
             ra, dec = coord.to_string('hmsdms').split()
-    except NameResolveError:
+    except name_resolve.NameResolveError:
         raise ValueError(query)
 
     return {'name': query, 'ra': ra, 'dec': dec, 'frame': frame}
 
 
-def get_local(query, pattern='*.toml', searchdirs=('.',), **kwargs):
+def from_local(query, pattern='*.toml', searchdirs=('.',), **kwargs):
     for searchdir in (Path(d).expanduser() for d in searchdirs):
         for path in searchdir.glob(pattern):
             data = azely.read_toml(path)
