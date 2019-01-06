f9a4727fcdcb5de79a8a307ce36fcd5fd1e941f5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-01-07 01:02:59 +0900

    :art: Refactoring

--- azely/__init__.py
+++ azely/__init__.py
@@ -2,8 +2,34 @@ __version__ = '0.3'
 __author__  = 'astropenguin'
 
 
-# submodules
+def _load_config(config='config.toml'):
+    # standard library
+    from collections import defaultdict
+    from logging import getLogger
+    from pathlib import Path
+    logger = getLogger(__name__)
+
+    # dependent packages
+    import toml
+
+    data = Path(__path__[0]) / 'data'
+    user = Path.home() / '.azely'
+
+    if not user.exists():
+        logger.info(f'creating {user}')
+        user.mkdir(parents=True)
+
+    if not (user/config).exists():
+        logger.info(f'creating {user/config}')
+        copy(data/config, user/config)
+
+    with (user/config).open() as f:
+        return defaultdict(dict, toml.load(f))
+
+
+config = _load_config()
 from . import utils
-from .config import *
+from . import query
+from . import azel
 from .query import *
 from .azel import *
