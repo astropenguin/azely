118e7d3f07eb86051f15c66ca73b7bee13768830
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-04 22:57:00 +0900

    Add startup script

---
+++ startup.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# standard library
+import os
+import re
+import sys
+import warnings
+from pathlib import Path
+
+# dependent packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
