de62fe2f5bddfd45a3eedd28cb3f2240c2a0d274
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-06 21:43:34 +0900

    Update module (time)

--- azely/time.py
+++ azely/time.py
@@ -1,40 +1,55 @@
-__all__ = ["get_time", "get_sidereal_time"]
+__all__ = ["get_time"]
 
 # standard library
 
 # dependent packages
 import pandas as pd
 from astropy.time import Time
-from dateutil.parser import parse
-from pandas import DataFrame
+from dateutil.parser import ParserError, parse
+from pandas import DatetimeIndex, Series
 from . import AzelyError, config
-from .location import get_location, get_tzinfo, get_earthloc
+from .location import get_earthloc, get_location, get_tzinfo
 from .utils import set_defaults
 
 # constants
 NOW = "now"
+TODAY = "today"
 HERE = "here"
 UTC = "utc"
+LST = "local sidereal time"
 PERIOD_SEP = ":"
 
 
 # main functions
 @set_defaults(**config["time"])
-def get_time(query: str = NOW, at: str = HERE, freq: str = "10T") -> DataFrame:
+def get_time(query: str = NOW, at: str = HERE, freq: str = "10T") -> Series:
+    index = get_time_index(query, at, freq)
+    time = Time(index.tz_convert(UTC), location=get_earthloc(at))
+
+    lst = pd.to_timedelta(time.sidereal_time("mean").value, unit="h")
+    return Series(lst, index, name=LST)
+
+
+# helper functions
+def get_time_index(query: str, at: str, freq: str) -> DatetimeIndex:
     tzinfo = get_tzinfo(at)
+    name = get_location(at).name
 
     if query == NOW:
         start = end = pd.Timestamp.now(tzinfo)
+    elif query == TODAY:
+        start = pd.Timestamp.now(tzinfo).date()
+        end = start + pd.offsets.Day()
     elif PERIOD_SEP in query:
-        start, end = map(parse, query.split(PERIOD_SEP))
+        try:
+            start, end = map(parse, query.split(PERIOD_SEP))
+        except ParserError:
+            raise AzelyError(f"Failed to parse: {query}")
     else:
-        start, end = parse(query), parse(query) + pd.offsets.Day()
+        try:
+            start = parse(query)
+            end = start + pd.offsets.Day()
+        except ParserError:
+            raise AzelyError(f"Failed to parse: {query}")
 
-    time = pd.date_range(start, end, None, freq, tz=tzinfo)
-    return DataFrame({tzinfo.zone: time}, index=time.tz_convert(UTC))
-
-
-def get_sidereal_time(query: str = NOW, at: str = HERE, freq: str = "10T") -> DataFrame:
-    pass
-
-
+    return pd.date_range(start, end, None, freq, tzinfo, name=name)
