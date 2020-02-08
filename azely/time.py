41b0b0cf63cd0f7f9ba8c8558f0313afe830fece
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-08 14:16:55 +0900

    Update module (time)

--- azely/time.py
+++ azely/time.py
@@ -6,9 +6,10 @@ from datetime import datetime, timedelta, tzinfo
 
 
 # dependent packages
+import pytz
 from dateutil.parser import ParserError, parse
 from pandas import DatetimeIndex, date_range
-from pytz import UnknownTimeZoneError, timezone
+from pytz import UnknownTimeZoneError
 from . import AzelyError, HERE, NOW, TODAY, config
 from .location import get_location
 from .utils import set_defaults
@@ -28,16 +29,22 @@ class Time(DatetimeIndex):
 
 # main functions
 @set_defaults(**config["time"])
-def get_time(query: str = NOW, view: str = HERE, freq: str = "10T") -> Time:
-    tzinfo = get_tzinfo(view)
+def get_time(
+    query: str = NOW,
+    view: str = HERE,
+    freq: str = "10T",
+    sep: str = "to",
+    timeout: int = 5,
+) -> Time:
+    tzinfo = get_tzinfo(view, timeout)
 
     if query == NOW:
         start = end = datetime.now(tzinfo)
     elif query == TODAY:
         start = datetime.now(tzinfo).date()
         end = start + timedelta(days=1)
-    elif PERIOD_SEP in query:
-        queries = query.split(PERIOD_SEP)
+    elif sep in query:
+        queries = query.split(sep)
         start, end = map(get_datetime, queries)
     else:
         start = get_datetime(query)
@@ -47,11 +54,11 @@ def get_time(query: str = NOW, view: str = HERE, freq: str = "10T") -> Time:
 
 
 # helper functions
-def get_tzinfo(query: str) -> tzinfo:
+def get_tzinfo(query: str, timeout: int) -> tzinfo:
     try:
-        return timezone(query)
+        return pytz.timezone(query)
     except UnknownTimeZoneError:
-        return timezone(get_location(query).timezone)
+        return get_location(query, timeout).tzinfo
 
 
 def get_datetime(query: str) -> datetime:
