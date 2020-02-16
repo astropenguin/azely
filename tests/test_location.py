05165efdbc653b97550ffcff3c15b17aa406fe6c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-16 21:47:27 +0900

    #7 Add test functions

--- tests/test_location.py
+++ tests/test_location.py
@@ -1,9 +1,14 @@
+# standard library
+from tempfile import NamedTemporaryFile
+
+
 # dependent packages
-import azely
+from azely.location import Location, get_location
+from toml import dump
 
 
 # constants
-expected = azely.location.Location(
+expected = Location(
     name="Array Operations Site",
     longitude="-67.75392913062831",
     latitude="-23.02306575",
@@ -12,5 +17,16 @@ expected = azely.location.Location(
 
 
 # test functions
-def test_location():
-    assert azely.get_location(expected.name) == expected
+def test_location_by_query():
+    assert get_location(expected.name) == expected
+
+
+def test_location_by_user():
+    with NamedTemporaryFile("w", suffix=".toml") as f:
+        name = "AOS"
+        query = f"{f.name}:{name}"
+
+        dump({name: expected.to_dict()}, f)
+        f.seek(0)
+
+        assert get_location(query) == expected
--- tests/test_object.py
+++ tests/test_object.py
@@ -1,21 +1,35 @@
+# standard library
+from tempfile import NamedTemporaryFile
+
+
 # dependent packages
-import azely
+from azely.object import Object, get_object
+from toml import dump
 
 
 # constants
-expected_solar = azely.object.Object(
-    name="Sun", frame="solar", longitude="NaN", latitude="NaN"
-)
+expected_solar = Object(name="Sun", frame="solar", longitude="NaN", latitude="NaN")
 
-expected_query = azely.object.Object(
+expected_icrs = Object(
     name="NGC1068", frame="icrs", longitude="02h42m40.771s", latitude="-00d00m47.84s",
 )
 
 
 # test functions
 def test_object_of_solar():
-    assert azely.get_object(expected_solar.name) == expected_solar
+    assert get_object(expected_solar.name) == expected_solar
 
 
 def test_object_by_query():
-    assert azely.get_object(expected_query.name) == expected_query
+    assert get_object(expected_icrs.name) == expected_icrs
+
+
+def test_location_by_user():
+    with NamedTemporaryFile("w", suffix=".toml") as f:
+        name = "AOS"
+        query = f"{f.name}:{name}"
+
+        dump({name: expected_icrs.to_dict()}, f)
+        f.seek(0)
+
+        assert get_object(query) == expected_icrs
