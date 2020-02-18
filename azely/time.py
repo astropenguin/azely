dc200b3a7b3e82f86b9e981ae2d9f9c3088f37f2
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-18 23:42:27 +0900

    #5 Add docstring to functions

--- azely/time.py
+++ azely/time.py
@@ -71,14 +71,18 @@ DELIMITER = "to"
 
 # data classes
 class Time(DatetimeIndex):
+    """Azely's time information class."""
+
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls, *args, **kwargs)
 
     def to_obstime(self, earthloc: EarthLocation) -> ObsTime:
+        """Convert it to an astropy's time (obstime)."""
         utc_naive = self.tz_convert(utc).tz_localize(None)
         return ObsTime(utc_naive, location=earthloc)
 
     def to_index(self) -> DatetimeIndex:
+        """Convert it to a pandas' DatetimeIndex."""
         return DatetimeIndex(self)
 
 
@@ -91,6 +95,64 @@ def get_time(
     yearfirst: bool = YEARFIRST,
     timeout: int = TIMEOUT,
 ) -> Time:
+    """Get time information by various ways.
+
+    Args:
+        query: Query string (e.g., `'2020-01-01 to 2020-01-05'`). If `'now'`
+            (by default) or `'today'` is specified, then current time
+            or time range today is computed, respectively.
+        view: Name of timezone (e.g., `'Asia/Tokyo'` or `'UTC'`) or location
+            with which timezone can be identified (e.g., `'Tokyo'`).
+        freq: Frequency of time samples as the same format of pandas' offset aliases
+            (e.g., `'1D'` -> 1 day, `'3H'` -> 3 hours, `'10T'` -> 10 minutes).
+        dayfirst: Whether to interpret the first value in an ambiguous 3-integer
+            date (e.g., `'01-02-03'`) as the day. If True, for example,
+            `'01-02-03'` is treated as Feb. 1st 2003.
+        yearfirst: Whether to interpret the first value in an ambiguous 3-integer
+            date (e.g., `'01-02-03'`) as the year. If True, for example,
+            `'01-02-03'` is treated as Feb. 3rd 2001. If `dayfirst` is also `True`,
+            then it will be Mar. 2nd 2001.
+        timeout: Query timeout expressed in units of seconds (see notes).
+
+    Returns:
+        time: Time information as an instance of `Time` class.
+
+    The `get_time` function computes time information in several cases:
+    (1) Current time (e.g., [2020-01-01 22:32:58+09:00]).
+    (2) Time range of today (e.g., [2020-01-01 00:00, ..., 2020-01-02 00:00])
+    (3) Time range of given date and length (by query)
+
+    In the cases of (1) and (2), special queries, `'now'` and `'today'`, must be
+    specified, respectively. The `view` option specifies a timezone where
+    time (range) is considered (timezone or location name can be accepted).
+
+    In the case of (3), formatted query (e.g., `'2020-01-01 to 2020-01-05'`)
+    or natural language-like query can be used (e.g., `'Jan. 1st to Jan. 5th'`),
+    where start and end must be separated by `'to'`. The `view` option also works.
+
+    Notes:
+        If location is specified, `azely.location.get_location` function
+        is used inside the function, which requires internet connection
+        if the location is queried for the first time.
+
+    Examples:
+        To get current time in Tokyo (by default)::
+
+            >>> time = azely.time.get_time('now', view="Tokyo")
+
+        To get current time in UTC::
+
+            >>> time = azely.time.get_time('now', view="UTC")
+
+        To get time range of today at ALMA AOS::
+
+            >>> time = azely.time.get_time('today', view="ALMA AOS")
+
+        To get time range from Jan. 1 to Jan. 5 in 2020 in UTC::
+
+            >>> time = azely.time.get_time('2020-01-01 to 2020-01-05', view='UTC')
+
+    """
     try:
         tzinfo = timezone(view)
     except UnknownTimeZoneError:
@@ -107,11 +169,13 @@ def get_time(
 
 # helper functions
 def get_time_now(tzinfo: tzinfo) -> DatetimeIndex:
+    """Get current time at given timezone."""
     start = end = datetime.now(tzinfo)
     return date_range(start, end, tz=tzinfo, name=tzinfo.zone)
 
 
 def get_time_today(freq: str, tzinfo: tzinfo) -> DatetimeIndex:
+    """Get time range of today at given timezone."""
     start = datetime.now(tzinfo).date()
     end = start + timedelta(days=1)
     return date_range(start, end, None, freq, tz=tzinfo, name=tzinfo.zone)
@@ -120,6 +184,7 @@ def get_time_today(freq: str, tzinfo: tzinfo) -> DatetimeIndex:
 def get_time_period(
     query: str, freq: str, tzinfo: tzinfo, parser: Callable
 ) -> DatetimeIndex:
+    """Get time range of given date and length at given timezone."""
     period = query.split(DELIMITER)
 
     try:
