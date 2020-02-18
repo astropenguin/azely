9d4bb87703b40eb6fdc272fc457a919320d4c132
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-18 21:07:25 +0900

    #5 Add module docstring (object)

--- azely/object.py
+++ azely/object.py
@@ -1,3 +1,43 @@
+"""Azely's object module.
+
+This module mainly provides (1) `Object` class for information of an astronomical
+object (object information, hereafter) and (2) `get_object` function to search for
+object information and get it as an instance of `Object` class.
+
+Object information is defiend as
+`Object(name: str, frame: str, longitude: str, latitude: str)`,
+where frame is a name of equatorial coordinates (e.g., icrs) and lon/lat are values
+of coordinates which must be written with units like 02h42m40.771s/-00d00m47.84s.
+
+Object info can be retrieved by the following two ways:
+(1) Data from CDS (by default). Internet connection is required.
+(2) User-defined object information written in a TOML file.
+
+In the case of (1), obtained object information is cached in a special
+TOML file (`~/.config/azely/object.toml`) for an offline use.
+
+Examples:
+    To get object info from CDS::
+
+        >>> obj = azely.object.get_object('NGC1068')
+
+    To get object info from a user-defined TOML file::
+
+        >>> obj = azely.object.get_object('user:GC')
+
+The second example assumes that a TOML file, `user.toml`, exists in a
+current directory or in the Azely's config directory (`~/.config/azely`)
+and the following TOML text is written in it::
+
+    [GC]
+    name = "Galactic center"
+    frame = "galactic"
+    longitude = "0deg"
+    latitude = "0deg"
+
+"""
+
+
 # standard library
 from dataclasses import asdict, dataclass
 from typing import Dict
