63b09542050dfa9eb991a8504015079f63173de9
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-18 23:42:12 +0900

    #5 Add module docstring (time)

--- azely/time.py
+++ azely/time.py
@@ -1,3 +1,45 @@
+"""Azely's time module.
+
+This module mainly provides `Time` class for date and time information at a
+given location (time information, hereafter) and `get_time` function to obtain
+time information as an instance of `Time` class.
+
+The `Time` class is subclass of `pandas.DatetimeIndex` and expressed like:
+`Time(['2020-02-18'], dtype='datetime64[ns, Asia/Tokyo]', name='Asia/Tokyo', freq='D')`.
+
+The `get_time` function computes time information in several cases:
+(1) Current time (e.g., [2020-01-01 22:32:58+09:00]).
+(2) Time range today (e.g., [2020-01-01 00:00, ..., 2020-01-02 00:00])
+(3) Time range of given date and length (by query)
+
+In the cases of (1) and (2), special queries, `'now'` and `'today'`, must be
+specified, respectively. The `view` option specifies a timezone where
+time (range) is considered (timezone or location name can be accepted).
+
+In the case of (3), formatted query (e.g., `'2020-01-01 to 2020-01-05'`)
+or natural language-like query can be used (e.g., `'Jan. 1st to Jan. 5th'`),
+where start and end must be separated by `'to'`. The `view` option also works.
+
+Examples:
+    To get current time in Tokyo (by default)::
+
+        >>> time = azely.time.get_time('now', view="Tokyo")
+
+    To get current time in UTC::
+
+        >>> time = azely.time.get_time('now', view="UTC")
+
+    To get time range today at ALMA AOS::
+
+        >>> time = azely.time.get_time('today', view="ALMA AOS")
+
+    To get time range from Jan. 1 to Jan. 5 in 2020 in UTC::
+
+        >>> time = azely.time.get_time('2020-01-01 to 2020-01-05', view='UTC')
+
+"""
+
+
 # standard library
 from datetime import datetime, timedelta, tzinfo
 from functools import partial
