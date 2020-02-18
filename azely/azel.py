973e6352a62651e3ecd91138b3229bbb5f3136b1
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-19 02:47:54 +0900

    #5 Add docstrings to functions (azel)

--- azely/azel.py
+++ azely/azel.py
@@ -109,6 +109,63 @@ def compute(
     yearfirst: bool = YEARFIRST,
     timeout: int = TIMEOUT,
 ) -> DataFrame:
+    """Compute az/el and local sidereal time (LST) of an astronomical object.
+
+    Args:
+        object: Query string for object information (e.g., `'Sun'` or `'NGC1068'`).
+            Spacify `'user:NGC1068'` if users want to get information from `user.toml`.
+        site: Query string for location information at a site (e.g., `'Tokyo'`).
+            Spacify `'user:Tokyo'` if users want to get information from `user.toml`.
+        time: Query string for time information at a view (e.g., `'2020-01-01'`).
+        view: Query string for timezone information at the view. (e.g., `'Asia/Tokyo'`,
+            `'UTC'`, or `Tokyo`). By default (`''`),  timezone at the site is used.
+        frame: (object option) Name of equatorial coordinates used in astropy's SkyCoord.
+        freq: (time option) Frequency of time samples as the same format of pandas' offset
+            aliases (e.g., `'1D'` -> 1 day, `'3H'` -> 3 hours, `'10T'` -> 10 minutes).
+        dayfirst: (time option) Whether to interpret the first value in an ambiguous
+            3-integer date (e.g., `'01-02-03'`) as the day. If True, for example,
+            `'01-02-03'` is treated as Feb. 1st 2003.
+        yearfirst: (time option) Whether to interpret the first value in an ambiguous
+            3-integer date (e.g., `'01-02-03'`) as the year. If True, for example,
+            `'01-02-03'` is treated as Feb. 3rd 2001. If `dayfirst` is also `True`,
+            then it will be Mar. 2nd 2001.
+        timeout: (common option) Query timeout expressed in units of seconds.
+
+    Returns:
+        dataframe: Computed objects' az/el and LST at given site and view.
+
+    Raises:
+        AzelyError: Raised if one of mid-level APIs fails to get any information.
+
+    The `compute` function (1) gets object, location, and time information, (2) computes
+    az/el and LST (local sidereal time), and (3) returns them as a pandas' DataFrame.
+
+    Object information can be retrieved either online (CDS) or offline (an user-defined
+    TOML file) by query (e.g., `'NGC1068'` or `'Sun'`). Location information can be
+    retrieved either online (IP address or OpenStreetMap) or offline (an user-defined
+    TOML file) by query (e.g., `'Tokyo'` or `'ALMA AOS'`). Time information can be
+    computed from either formatted (e.g., `'2020-01-01'`) and natural language-like
+    query (e.g., `'Jan 1st 2020'`). See docstrings of `get_[object|location|time]`
+    functions for more detailed query options.
+
+    There are two different locations to be used for a computation:
+    (1) `site`: location where az/el of an object is computed.
+    (2) `view`: location where time information (timezone) is considered.
+
+    Examples:
+        To compute daily az/el of NGC1068 at ALMA AOS::
+
+            >>> df = azely.compute('NGC1068', 'ALMA AOS', '2020-02-01')
+
+        To compute the same object and location but view from Japan::
+
+            >>> df = azely.compute('NGC1068', 'ALMA AOS', '2020-02-01', view='Tokyo')
+
+        To compute az/el of Sun at noon during an year at Tokyo::
+
+            >>> df = azely.compute('Sun', 'Tokyo', '1/1 12:00 to 12/31 12:00', freq='1D')
+
+    """
     object_ = get_object(object, frame, timeout)
     site_ = get_location(site, timeout)
     time_ = get_time(time, view or site, freq, dayfirst, yearfirst, timeout)
@@ -118,6 +175,23 @@ def compute(
 
 # helper functions
 def compute_from(object: Object, site: Location, time: Time) -> DataFrame:
+    """Compute az/el and local sidereal time (LST) of an astronomical object.
+
+    Similar to `compute` function, but this function receives instances
+    of `Object`, `Location`, and `Time` classes as arguments.
+
+    Args:
+        object: Object information.
+        site: Site location information.
+        time: Time information.
+
+    Returns:
+        dataframe: Computed objects' az/el and LST at given site and view.
+
+    Raises:
+        AzelyError: Raised if one of mid-level APIs fails to get any information.
+
+    """
     obstime = time.to_obstime(site.to_earthloc())
     skycoord = object.to_skycoord(obstime)
 
