bcaf6c442b5be0ffddf6ab6b426a2c78d84fbfa7
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-02-19 00:45:00 +0900

    #5 Add docstrings to functions (utils)

--- azely/utils.py
+++ azely/utils.py
@@ -3,7 +3,7 @@
 This module mainly provides lower-level utilities:
 (1) `AzelyError` class as Azely's exception class
 (2) `open_toml` function to open (and update if any) a TOML file
-(3) `cache_to` decorator which caches a return of a function to a TOML file
+(3) `cache_to` decorator which caches returns of a function to a TOML file
 (4) `set_defaults` decorator which replaces default values of a function
 
 """
@@ -35,6 +35,39 @@ class AzelyError(Exception):
 
 
 def open_toml(path: PathLike, alt_dir: PathLike = "."):
+    """Open a TOML file and get contents as a dictionary.
+
+    If this function is used in a `with` context management,
+    any updates to the dictionary is also reflected on the TOML file.
+
+    Args:
+        path: Path or filename (without suffix) of a TOML file.
+            If the latter is specified and if it does not exist in a current
+            directory, then the function tries to find it in `alt_dir`.
+        alt_dir: Path of a directory where the function tries to find
+            the TOML file if it does not exist in a current directory.
+
+    Returns:
+        toml_dict: Dictionary equivalent to the contents of the TOML file.
+
+    Raises:
+        AzelyError: Raised if the TOML file is not found anywhere.
+
+    Examples:
+        To simply open a TOML file (for example, `./user.toml`)::
+
+            >>> dic = azely.utils.open_toml('user.toml')
+
+        or::
+
+            >>> dic = azely.utils.open_toml('user')
+
+        To open and update a TOML file::
+
+            >>> with azely.utils.open_toml('user.toml') as dic:
+            ...     dic['new_key'] = new_value
+
+    """
     path = Path(path).with_suffix(TOML_SUFFIX).expanduser()
 
     if not path.exists():
@@ -47,6 +80,36 @@ def open_toml(path: PathLike, alt_dir: PathLike = "."):
 
 
 class cache_to:
+    """Decorator which cache returns of a function to a TOML file.
+
+    Suppose there is a function which takes a long time to get
+    a result and it is decorated by this function::
+
+        >>> import time
+        >>> @azely.utils.cache_to('cache.toml')
+        ... def func(query: str) -> str:
+        ...     # simulates a long-time processing
+        ...     time.sleep(10)
+        ...     return query + '!'
+
+    Then the following function calls::
+
+        >>> func('aaa')
+        >>> func('bbb')
+        >>> func('ccc')
+
+    would take ~30 seconds to finish. But at the same time,
+    the results are cached to `cache.toml` like::
+
+        aaa = "aaa!"
+        bbb = "bbb!"
+        ccc = "ccc!"
+
+    Then the second calls would take much shorter time because
+    cached values are simply read and returned by the decorator.
+
+    """
+
     def __init__(self, path: PathLike, query: str = "query") -> None:
         self.path = ensure_existance(path)
         self.query = query
@@ -71,6 +134,34 @@ class cache_to:
 
 
 class set_defaults:
+    """Decorator which replaces default values of a function.
+
+    The alternative defualt values are read from a TOML file.
+    Suppose there is a function with some parameters
+    and it is decorated by this function::
+
+        >>> @azely.utils.set_defaults('defaults.toml')
+        ... def func(a: int, b: int = 0) -> int:
+        ...     return a + b
+
+    Suppose the contents of `defaults.toml` is like::
+
+        a = 1
+        b = 2
+
+    Then the following function calls results in::
+
+        >>> func(0, 1) # -> 1
+        >>> func(0) # -> 2
+        >>> func() # -> 3
+
+    This is because now the function is equivalent to::
+
+        >>> def func(a: int = 1, b: int = 2) -> int:
+        ...     return a + b
+
+    """
+
     def __init__(self, path: PathLike, key: str = "") -> None:
         self.path = ensure_existance(path)
         self.key = key
@@ -109,6 +200,7 @@ class set_defaults:
 
 # helper classes/functions
 def ensure_existance(path: PathLike) -> Path:
+    """Make path of file exist."""
     path = Path(path)
 
     path.parent.mkdir(parents=True, exist_ok=True)
@@ -118,6 +210,8 @@ def ensure_existance(path: PathLike) -> Path:
 
 
 class TOMLDict(dict):
+    """Open and update a TOML file as a dictionary."""
+
     def __init__(self, path: PathLike) -> None:
         self.path = Path(path)
         super().__init__(self.load_toml())
