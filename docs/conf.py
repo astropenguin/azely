130d157b7746f3b132033f1070f6cfa5daea9d61
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2022-11-03 08:44:08 +0000

    #41 Fix docs' config

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,3 @@
-# Project information
-project = "Azely"
-copyright = "2017-2020, Akio Taniguchi"
-author = "Akio Taniguchi"
-release = "0.7.0"
-
-
-# General configuration
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
-]
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# Options for HTML output
-html_static_path = ["_static"]
-html_logo = "_static/logo.svg"
-html_theme = "pydata_sphinx_theme"
-html_theme_options = {
-    "github_url": "https://github.com/astropenguin/azely/",
-    "twitter_url": "https://twitter.com/astropengu_in/",
-}
 # project information
 author = "Akio Taniguchi"
 copyright = "2017-2022, Akio Taniguchi"
