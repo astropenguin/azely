e3e53983f5aa8636fffb8d65985c66deb8ef096b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-05-04 15:27:29 +0000

    #47 Update consts module to ensure existence of files

--- azely/consts.py
+++ azely/consts.py
@@ -22,32 +22,54 @@ __all__ = [
 
 
 # standard library
-from os import environ, getenv
+from os import environ
 from pathlib import Path
 
 
 # constants (static)
-DAYFIRST = False  #: Default value for the ``dayfirst`` argument.
-FRAME = "icrs"  #: Default value for the ``frame`` argument.
-FREQ = "10T"  #: Default value for the ``freq`` argument.
-HERE = "here"  #: Special value for getting location information by current IP address.
-NOW = "now"  #: Special value for getting current time information.
-TIMEOUT = 10  #: Default value for the ``timeout`` argument.
-TODAY = "today"  #: Special value for getting today's time information.
-YEARFIRST = False  #: Default value for the ``yearfirst`` argument.
+DAYFIRST = False
+"""Default value for the ``dayfirst`` argument."""
+
+FRAME = "icrs"
+"""Default value for the ``frame`` argument."""
+
+FREQ = "10T"
+"""Default value for the ``freq`` argument."""
+
+HERE = "here"
+"""Special value for getting location information by current IP address."""
+
+NOW = "now"
+"""Special value for getting current time information."""
+
+TIMEOUT = 10
+"""Default value for the ``timeout`` argument."""
+
+TODAY = "today"
+"""Special value for getting today's time information."""
+
+YEARFIRST = False
+"""Default value for the ``yearfirst`` argument."""
 
 
 # constants (dynamic)
-def get_azely_dir() -> Path:
-    if "AZELY_DIR" in environ:
-        return Path(getenv("AZELY_DIR"))
-    elif "XDG_CONFIG_HOME" in environ:
-        return Path(getenv("XDG_CONFIG_HOME")) / "azely"
-    else:
-        return Path().home() / ".config" / "azely"
+def ensure(file: Path) -> Path:
+    """Create an empty file if it does not exist."""
+    if not file.exists():
+        file.parent.mkdir(parents=True, exist_ok=True)
+        file.touch()
+
+    return file
 
 
-AZELY_DIR = get_azely_dir()
-AZELY_CONFIG = AZELY_DIR / "config.toml"
-AZELY_OBJECT = AZELY_DIR / "objects.toml"
-AZELY_LOCATION = AZELY_DIR / "locations.toml"
+if "AZELY_DIR" in environ:
+    AZELY_DIR = Path(environ["AZELY_DIR"])
+elif "XDG_CONFIG_HOME" in environ:
+    AZELY_DIR = Path(environ["XDG_CONFIG_HOME"]) / "azely"
+else:
+    AZELY_DIR = Path().home() / ".config" / "azely"
+
+
+AZELY_CONFIG = ensure(AZELY_DIR / "config.toml")
+AZELY_OBJECT = ensure(AZELY_DIR / "objects.toml")
+AZELY_LOCATION = ensure(AZELY_DIR / "locations.toml")
