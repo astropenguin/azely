3a0524b1b4ff99478664def2da5f202488f6d68b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-08-01 08:03:44 +0000

    #66 Update tests for location and object module

--- tests/test_location.py
+++ tests/test_location.py
@@ -5,26 +5,40 @@ from tempfile import NamedTemporaryFile
 
 # dependencies
 from azely.location import Location, get_location
+from pytest import mark
 from tomlkit import dump
 
 
 # test data
-expected = Location(
-    name="Array Operations Site",
-    longitude="292d14m45.85512974s",
-    latitude="-23d01m23.0367s",
-    altitude="0.0 m",
-)
+locations = [
+    Location(
+        name="Atacama Large Millimeter/submillimeter Array",
+        longitude="292d14m48.93972s",
+        latitude="-23d01m21.97704s",
+        altitude="0.0 m",
+    ),
+    Location(
+        name="Gran Telescopio Milimétrico Alfonso Serrano",
+        longitude="262d41m06.76068s",
+        latitude="18d59m08.66328s",
+        altitude="0.0 m",
+    ),
+    Location(
+        name="Nobeyama Radio Astronomy Observatory",
+        longitude="138d28m25.28621699s",
+        latitude="35d56m34.76364s",
+        altitude="0.0 m",
+    ),
+]
 
 
 # test functions
-def test_location_by_query():
-    assert get_location(expected.name, update=True) == expected
-
-
-def test_location_by_user():
+@mark.parametrize("obj", locations)
+def test_get_location(obj: Location) -> None:
     with NamedTemporaryFile("w", suffix=".toml") as f:
-        dump({expected.name: asdict(expected)}, f)
-        f.seek(0)
+        dump({obj.name: asdict(obj)}, f)
 
-        assert get_location(expected.name, source=f.name) == expected
+        # save an object to the TOML file
+        assert get_location(obj.name, source=f.name) == obj
+        # read the object from the TOML file
+        assert get_location(obj.name, source=f.name) == obj
--- tests/test_object.py
+++ tests/test_object.py
@@ -5,37 +5,40 @@ from tempfile import NamedTemporaryFile
 
 # dependencies
 from azely.object import Object, get_object
+from pytest import mark
 from tomlkit import dump
 
 
 # test data
-expected_solar = Object(
-    name="Sun",
-    longitude="NA",
-    latitude="NA",
-    frame="solar",
-)
-
-expected_icrs = Object(
-    name="3C 273",
-    longitude="12h29m06.69982572s",
-    latitude="2d03m08.59762998s",
-    frame="icrs",
-)
+objects = [
+    Object(
+        name="Sun",
+        longitude="NA",
+        latitude="NA",
+        frame="solar",
+    ),
+    Object(
+        name="3C 273",
+        longitude="12h29m06.69982572s",
+        latitude="2d03m08.59762998s",
+        frame="icrs",
+    ),
+    Object(
+        name="3C 345",
+        longitude="16h42m58.80997043s",
+        latitude="39d48m36.9939552s",
+        frame="icrs",
+    ),
+]
 
 
 # test functions
-def test_object_of_solar():
-    assert get_object(expected_solar.name, update=True) == expected_solar
-
-
-def test_object_by_query():
-    assert get_object(expected_icrs.name, update=True) == expected_icrs
-
-
-def test_object_by_user():
+@mark.parametrize("obj", objects)
+def test_get_object(obj: Object) -> None:
     with NamedTemporaryFile("w", suffix=".toml") as f:
-        dump({expected_icrs.name: asdict(expected_icrs)}, f)
-        f.seek(0)
+        dump({obj.name: asdict(obj)}, f)
 
-        assert get_object(expected_icrs.name, source=f.name) == expected_icrs
+        # save an object to the TOML file
+        assert get_object(obj.name, source=f.name) == obj
+        # read the object from the TOML file
+        assert get_object(obj.name, source=f.name) == obj
