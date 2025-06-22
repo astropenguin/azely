ea56abc8f4b0f4c001285a223d0f7431cce71516
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-06-22 15:33:46 +0000

    #80 Update tests for location module

--- tests/test_location.py
+++ tests/test_location.py
@@ -15,24 +15,29 @@ locations = [
         name="Atacama Large Millimeter/submillimeter Array",
         longitude="-67d45m11.06028s",
         latitude="-23d01m21.97704s",
-        altitude="-3.8089945457383293e-10 m",
+        altitude="0.0 m",
     ),
     Location(
         name="Nobeyama Radio Astronomy Observatory",
         longitude="138d28m25.28616s",
         latitude="35d56m34.76364s",
-        altitude="2.1695119188480857e-11 m",
+        altitude="0.0 m",
     ),
 ]
 
 
 # test functions
-@mark.parametrize("obj", locations)
-def test_get_location(obj: Location) -> None:
+@mark.parametrize("expected", locations)
+def test_get_location(expected: Location, /) -> None:
     with NamedTemporaryFile("w", suffix=".toml") as f:
-        dump({obj.name: asdict(obj)}, f)
+        dump({expected.name: asdict(expected)}, f)
 
         # save an object to the TOML file
-        assert get_location(obj.name, source=f.name) == obj
+        location = get_location(expected.name, source=f.name)
+        assert location.longitude == expected.longitude
+        assert location.latitude == expected.latitude
+
         # read the object from the TOML file
-        assert get_location(obj.name, source=f.name) == obj
+        location = get_location(expected.name, source=f.name)
+        assert location.longitude == expected.longitude
+        assert location.latitude == expected.latitude
